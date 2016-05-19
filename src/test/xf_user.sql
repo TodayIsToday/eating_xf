3a0a38d4ecec54cd11eadd0508b9b9cde0aea2e3
huangkai <huangkai@wondersgroup.com>
2016-05-19 23:09:35 +0800

    用户表初始化数据

---
+++ src/test/xf_user.sql
@@ -0,0 +1,24 @@
+/*
+SQLyog Professional v12.08 (64 bit)
+MySQL - 5.7.12-log 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `xf_user` (
+	`xf_user_uuid` varchar (150),
+	`user_name` varchar (150),
+	`password` varchar (150),
+	`login_name` varchar (150),
+	`moblie_phone` varchar (90),
+	`email` varchar (150)
+); 
+insert into `xf_user` (`xf_user_uuid`, `user_name`, `password`, `login_name`, `moblie_phone`, `email`) values('b1c3c31b-1b82-11e6-bfe2-3c970e8624cf','张雄','123456',NULL,NULL,NULL);
+insert into `xf_user` (`xf_user_uuid`, `user_name`, `password`, `login_name`, `moblie_phone`, `email`) values('b1c3c31b-1b82-11e6-bfe5-3c270e8624cf','何亚军','123456','hyj',NULL,NULL);
+insert into `xf_user` (`xf_user_uuid`, `user_name`, `password`, `login_name`, `moblie_phone`, `email`) values('b1c3c31b-1b82-11e6-bfe5-3c970e8624cf','黄凯','123456','hk','15821031446','huangkai@wondersgroup.com');
+insert into `xf_user` (`xf_user_uuid`, `user_name`, `password`, `login_name`, `moblie_phone`, `email`) values('b1c3c31b-1b82-11e6-bfe5-3c970ed624cf','沈涛','123456',NULL,NULL,NULL);
+insert into `xf_user` (`xf_user_uuid`, `user_name`, `password`, `login_name`, `moblie_phone`, `email`) values('b1c3c31b-1b82-11e6-bfe5-3c970t8624cf','岳安然','123456',NULL,NULL,NULL);
+insert into `xf_user` (`xf_user_uuid`, `user_name`, `password`, `login_name`, `moblie_phone`, `email`) values('b1c3c31b-1b82-11e6-bfe5-3c9e0e8624cf','吕排正','123456',NULL,NULL,NULL);
+insert into `xf_user` (`xf_user_uuid`, `user_name`, `password`, `login_name`, `moblie_phone`, `email`) values('b1c3c31b-1b82-11e6-bfe5-3c9e0ej624cf','刘俊杰','123456',NULL,NULL,NULL);
+insert into `xf_user` (`xf_user_uuid`, `user_name`, `password`, `login_name`, `moblie_phone`, `email`) values('b1c3c31b-1b82-11e6-bfe5-3c9e0ej62dcf','陆海龙','123456',NULL,NULL,NULL);
+insert into `xf_user` (`xf_user_uuid`, `user_name`, `password`, `login_name`, `moblie_phone`, `email`) values('b1c3c31b-1b82-11e6-bfe5-rc9e0ej62dcf','任','123456',NULL,NULL,NULL);
